0b55e356acd5ab92f9e6259a629ab7376814be89
Lindsey Laveirge <llaveirge@gmail.com>
2021-12-30 23:21:23 -0700

    Listen for arrow keydown events on the document and update car's data-direction attribute to correct direction. #2

--- js/main.js
+++ js/main.js
@@ -4,5 +4,18 @@ var data = {
 
 var $car = document.getElementById('car');
 var $carDirection = $car.getAttribute('data-direction');
-
 data.direction = $carDirection;
+
+function turnCar(event) {
+  if (event.code === 'ArrowLeft') {
+    $car.setAttribute('data-direction', 'west');
+  } else if (event.code === 'ArrowUp') {
+    $car.setAttribute('data-direction', 'north');
+  } else if (event.code === 'ArrowDown') {
+    $car.setAttribute('data-direction', 'south');
+  } else if (event.code === 'ArrowRight') {
+    $car.setAttribute('data-direction', 'east');
+  }
+}
+
+document.addEventListener('keydown', turnCar);
