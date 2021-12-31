e7ef12e32258ee95717a4ee6f9886bb4a590cfe5
Lindsey Laveirge <llaveirge@gmail.com>
2021-12-31 16:01:00 -0700

    Update data model direction property. #2

--- js/main.js
+++ js/main.js
@@ -9,12 +9,16 @@ data.direction = $carDirection;
 function turnCar(event) {
   if (event.code === 'ArrowLeft') {
     $car.setAttribute('data-direction', 'west');
+    data.direction = 'west';
   } else if (event.code === 'ArrowUp') {
     $car.setAttribute('data-direction', 'north');
+    data.direction = 'north';
   } else if (event.code === 'ArrowDown') {
     $car.setAttribute('data-direction', 'south');
+    data.direction = 'south';
   } else if (event.code === 'ArrowRight') {
     $car.setAttribute('data-direction', 'east');
+    data.direction = 'east';
   }
 }
 
